b572dc842fea7e521fca8f99c1949a7ada840da5
Sourabh <simplysourabh_123@yahoo.com>
2016-02-05 14:25:04 +0530

    modified gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -4,10 +4,17 @@
  * 
  * Extended GCD : Check if d is gcd of a and b
  * 		- Bezout's Identity : gcd(a, b) = ax + by  where x and y are integers
+ * 
+ * Modular mulplicative inverse using Bezout's identity:
+ * 		- two numbers a and m
+ * 		- let x be mult inverse of a : (a*x) % m == 1
+ * 		- gcd(a, m) = ax + my = 1 (a and m should be co prime else inv doesn't exist)
+ * 		- find x using bezout's identity
  */ 		
 
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long LL;
 
 int gcd1(int a, int b) {			//recursive
 	if (a == 0) return b;
@@ -25,21 +32,29 @@ int gcd2(int a, int b) {			//bottom up
 	return b;
 }	
 
-int ext_gcd(int a, int b, int &x, int &y) {
+LL ext_gcd(LL a, LL b, LL &x, LL &y) {
 	if (a == 0) {
 		x = 0; y = 1;
 		return b;
 	}
-	int x1, y1; 
-	int d = ext_gcd(b%a, a, x1, y1);
+	LL x1, y1; 
+	LL d = ext_gcd(b%a, a, x1, y1);
 	x = y1 - (b/a) * x1;
 	y = x1;
 	return d;	
 }	
-		
+
+LL mod_mult_inverse(LL a, LL m) {
+	//(a*x) % m == 1
+	LL x, y;
+	ext_gcd(a, m, x, y);	
+	if (x<0) x+=m;
+	return x;
+}		
 
 int main() {
 	int i, g, arr[] = {9, 27, 15, 45};
+	
 	g = arr[0];
 	for (i=1; i<4; ++i) g = gcd2(g, arr[i]); 
 	printf("g : %d\n", g);
@@ -50,5 +65,9 @@ int main() {
 	
 	printf("Extended gcd \n");
 	printf("g : %d, a : %d, x : %d,  b : %d, y : %d\n", g, a, x, b, y);
+	
+	//cout << "mod mult inverse  of 17 (m=3120): " << mod_mult_inverse(17, 3120) << endl;
+	 
+	
 	return 0;
 }	
